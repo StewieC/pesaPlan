ea3b3ef6627a68094e540ed96f876dea4491b91f
StewieC <makokha.stewart21@students.dkut.ac.ke>
2025-08-05 23:41:56 +0300

    debug, login system

--- pesaplan/urls.py
+++ pesaplan/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# pesa_plan/urls.py
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('finance.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('accounts.urls')),  # Your custom accounts app URLs
+    path('', include('finance.urls')),  # Finance app URLs
 ]
